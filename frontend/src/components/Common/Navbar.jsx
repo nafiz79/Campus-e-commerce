13de98d474e6273dbf4e0552213dcd6bbbe6dcf3
Nafees Ahamed <nafeesahamed@Nafeess-Mac-mini.local>
2025-03-29 11:08:41 +0600

    cart-drawer add

--- frontend/src/components/Common/Navbar.jsx
+++ frontend/src/components/Common/Navbar.jsx
@@ -5,8 +5,14 @@ import {
   HiBars3BottomRight,
 } from "react-icons/hi2";
 import SearchBar from "./SearchBar";
+import CartDrawer from "../Layout/CartDrawer";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const toggleCartDrawer = () => {
+    setDrawerOpen(!drawerOpen);
+  };
   return (
     <>
       <nav className="container mx-auto flex items-center justify-between py-4 px-6">
@@ -48,7 +54,10 @@ const Navbar = () => {
           <Link to="/profile" className="hover:text-black">
             <HiOutlineUser className="h-6 w-6 text-gray-700" />
           </Link>
-          <button className="relative hover:text-black">
+          <button
+            onClick={toggleCartDrawer}
+            className="relative hover:text-black"
+          >
             <HiOutlineShoppingBag className="h-6 w-6 text-gray-700" />
             <span className="absolute -top-1 bg-rabbit-red text-white text-xs rounded-full px-2 py-0.5">
               4
@@ -62,8 +71,10 @@ const Navbar = () => {
           <button className="md:hidden">
             <HiBars3BottomRight className="h-6 w-6 text-gray-700" />
           </button>
+          {/* Cart Drawer */}
         </div>
       </nav>
+      <CartDrawer drawerOpen={drawerOpen} toggleCartDrawer={toggleCartDrawer} />
     </>
   );
 };
