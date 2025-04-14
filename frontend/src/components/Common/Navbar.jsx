31e29c9d9f8b894a12da7836f893841978f532c5
Nafees Ahamed <nafeesahamed@Nafeess-Mac-mini.local>
2025-04-14 16:11:01 +0600

    filters and mobile sidebarFilters Adeed

--- frontend/src/components/Common/Navbar.jsx
+++ frontend/src/components/Common/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = () => {
         {/* Center -Navigation Links */}
         <div className="hidden md:flex space-x-6 ">
           <Link
-            to="/"
+            to="/collections/all"
             className="text-gray-700 hover:text-black text-sm font-medium uppercase"
           >
             MEN
@@ -97,7 +97,7 @@ const Navbar = () => {
           <h2 className="text-xl font-semibold mb-4">Menu</h2>
           <nav className="space-y-4">
             <Link
-              to="#"
+              to="/collections/all"
               onClick={toggleNavDrawer}
               className="block text-gray-600 hover:text-black"
             >
