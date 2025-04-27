5804050dff693107bd1e71a812b4e2b831b1f017
Nafees Ahamed <nafeesahamed@Nafeess-Mac-mini.local>
2025-04-27 15:24:51 +0600

    login route added

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -55,4 +55,47 @@ router.post("/register", async (req, res) => {
   }
 });
 
+// @route POST /api/user/login
+// @desc Authentic user
+// @access public
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    // Find the user by email
+    let user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "Invalid Credentials" });
+    const isMatch = await user.matchPassword(password);
+    if (!isMatch)
+      return res.status(400).json({ message: "Invalid Credentials" });
+
+    // Create JWT Payload
+
+    const payload = { user: { id: user._id, role: user.role } };
+
+    // Sign and return the token along with user data
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: "40h" },
+      (err, token) => {
+        if (err) throw err;
+
+        // Send the user and token in response
+        res.json({
+          user: {
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+          },
+          token,
+        });
+      }
+    );
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
