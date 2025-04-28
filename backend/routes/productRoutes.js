fb991f507c751ac9611c5466fcfa2cdba6057925
Nafees Ahamed <nafeesahamed@Nafeess-Mac-mini.local>
2025-04-29 03:53:08 +0600

    get product by id route added

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -236,4 +236,20 @@ router.get("/", async (req, res) => {
   }
 });
 
+// @route GET /api/products/:id
+// @desc Get a single product by ID
+router.get("/:id", async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({ message: "Product not found" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
