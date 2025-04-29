86f520afcfedb8565571494a2a90209b31c671e5
Nafees Ahamed <nafeesahamed@Nafeess-Mac-mini.local>
2025-04-29 16:22:31 +0600

    after adding product to checkout quantity updation route added and also if the product quantity is set to 0 then product is removed

--- backend/routes/cartRoutes.js
+++ backend/routes/cartRoutes.js
@@ -83,4 +83,47 @@ router.post("/", async (req, res) => {
     res.status(500).json({ message: "Server Error" });
   }
 });
+
+// @route PUT /api/cart
+// @desc Update product quantity in the cart for a guest or logged-in user
+// @access Public
+
+router.put("/", protect, async (req, res) => {
+  const { productId, quantity, size, color, guestId, userId } = req.body;
+  try {
+    let cart = await getCart(userId, guestId);
+    if (!cart) return res.status(404).json({ message: "Cart not found" });
+
+    const productIndex = cart.products.findIndex(
+      (p) =>
+        p.productId.toString() === productId &&
+        p.size === size &&
+        p.color === color
+    );
+
+    if (productIndex > -1) {
+      // Update quantity or remove item
+      if (quantity > 0) {
+        cart.products[productIndex].quantity = quantity;
+      } else {
+        cart.products.splice(productIndex, 1); // Remove product
+      }
+
+      // Recalculate total price
+      cart.totalPrice = cart.products.reduce(
+        (acc, item) => acc + item.price * item.quantity,
+        0
+      );
+
+      await cart.save();
+      return res.status(200).json(cart);
+    } else {
+      return res.status(404).json({ message: "Product not found in cart" });
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Server Error" });
+  }
+});
+
 module.exports = router;
