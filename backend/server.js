6a119fbdd36b3c64963b765a9934f1c156302981
Nafees Ahamed <nafeesahamed@Nafeess-Mac-mini.local>
2025-05-02 02:54:49 +0600

    upload route added

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const productRoutes = require("./routes/productRoutes");
 const cartRoutes = require("./routes/cartRoutes");
 const checkoutRoutes = require("./routes/checkoutRoutes");
 const orderRoutes = require("./routes/orderRoutes");
+const uploadRoutes = require("./routes/uploadRoutes");
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -28,6 +29,7 @@ app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/checkout", checkoutRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
