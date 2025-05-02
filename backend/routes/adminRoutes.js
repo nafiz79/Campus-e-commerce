3a49f6c062d947dbd2bd84a93d4d466a4ead5f26
Nafees Ahamed <nafeesahamed.tanmoy@gmail.com>
2025-05-03 03:36:00 +0600

    Adding a new user route added

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -15,4 +15,31 @@ router.get("/", protect, admin, async (req, res) => {
     res.status(500).json({ message: "Server Error" });
   }
 });
+
+// @route POST /api/admin/users
+// @desc Add a new user (admin only)
+// @access Private/admin
+router.post("/", protect, admin, async (req, res) => {
+  const { name, email, password, role } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    user = new User({
+      name,
+      email,
+      password,
+      role: role || "customer",
+    });
+    await user.save();
+    res.status(201).json({ message: "User Created successfully", user });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
 module.exports = router;
