1cef48511e7f5ddfb94069d5734851c5106d141f
Nafees Ahamed <nafeesahamed.tanmoy@gmail.com>
2025-05-03 04:01:06 +0600

    update and delete user route added

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -42,4 +42,41 @@ router.post("/", protect, admin, async (req, res) => {
   }
 });
 
+// @route PUT /api/admin/users/:id
+// @desc Update user info(admin only) - Name, email and role
+// @access Private/Admin
+router.put("/:id", protect, admin, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (user) {
+      user.name = req.body.name || user.name;
+      user.email = req.body.email || user.email;
+      user.role = req.body.role || user.role;
+    }
+    const updatedUser = await user.save();
+    res.json({ message: "User updated successfully", user: updatedUser });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
+// @route DELETE /api/admin/users:id
+// @desc Delete a user
+// @access Private/Admin
+router.delete("/:id", protect, admin, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (user) {
+      await user.deleteOne();
+      res.json({ message: "User deleted successfully" });
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
 module.exports = router;
